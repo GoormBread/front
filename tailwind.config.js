542294efb50a170bebf2e1a30811d48e8420f1cb
Park Sangyeol <tkddufdl77@ajou.ac.kr>
2024-05-18 16:11:47 +0900

    Chore: Add theme
    웹 사이트 제작을 위한 theme 항목 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'groom-brown': '#F5F0E5',
+        'groom-browntext': '#A1824A',
+        'groom-blacktext': '#1C170D'
+      },
+      spacing: {
+        '128': '32rem'
+      }
+    },
   },
   plugins: [],
 }
