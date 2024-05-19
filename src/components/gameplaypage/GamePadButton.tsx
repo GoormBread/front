de8bbfc56c4bd33d1c7b77faf41dc04ea9d6e00b
JungSunghoon <jsh120519@ajou.ac.kr>
2024-05-20 03:17:57 +0900

    chore: Change brightness of gamepadbutton

--- src/components/gameplaypage/GamePadButton.tsx
+++ src/components/gameplaypage/GamePadButton.tsx
@@ -32,31 +32,31 @@ export default function GamePadButton({ className }: GamePadButtonProps) {
     <div className={`${className}`}>
       <div className="flex flex-col items-center">
         <div className="flex justify-center -mb-7">
-          <img src="/GameplayPage/up_w.svg" className={`${activeButton === "arrowup" ? "filter brightness-90" : ""}`} />
+          <img src="/GameplayPage/up_w.svg" className={`${activeButton === "arrowup" ? "filter brightness-[0.85]" : ""}`} />
         </div>
         <div className="flex justify-center">
-          <img src="/GameplayPage/left_w.svg" className={`${activeButton === "arrowleft" ? "filter brightness-90" : ""}`} />
-          <img src="/GameplayPage/right_w.svg" className={`ml-1 ${activeButton === "arrowright" ? "filter brightness-90" : ""}`} />
+          <img src="/GameplayPage/left_w.svg" className={`${activeButton === "arrowleft" ? "filter brightness-[0.85]" : ""}`} />
+          <img src="/GameplayPage/right_w.svg" className={`ml-1 ${activeButton === "arrowright" ? "filter brightness-[0.85]" : ""}`} />
         </div>
         <div className="flex justify-center -mt-7">
-          <img src="/GameplayPage/down_w.svg" className={`${activeButton === "arrowdown" ? "filter brightness-90" : ""}`} />
+          <img src="/GameplayPage/down_w.svg" className={`${activeButton === "arrowdown" ? "filter brightness-[0.85]" : ""}`} />
         </div>
       </div>
       <div className="flex justify-center items-center ml-40">
         <text className="absolute font-serif font-bold text-lg z-10">SELECT</text>
-        <img src="/GameplayPage/box_w.svg" className={`${activeButton === "control" ? "filter brightness-90" : ""}`} />
+        <img src="/GameplayPage/box_w.svg" className={`${activeButton === "control" ? "filter brightness-[0.85]" : ""}`} />
       </div>
       <div className="flex justify-center items-center ml-4">
         <text className="absolute font-serif font-bold text-lg z-10">START</text>
-        <img src="/GameplayPage/box_w.svg" className={`${activeButton === "shift" ? "filter brightness-90" : ""}`} />
+        <img src="/GameplayPage/box_w.svg" className={`${activeButton === "shift" ? "filter brightness-[0.85]" : ""}`} />
       </div>
       <div className="flex justify-center items-center ml-10">
         <text className="absolute font-serif font-bold text-2xl z-10">A</text>
-        <img src="/GameplayPage/circle_w.svg" className={`${activeButton === "a" ? "filter brightness-90" : ""}`} />
+        <img src="/GameplayPage/circle_w.svg" className={`${activeButton === "a" ? "filter brightness-[0.85]" : ""}`} />
       </div>
       <div className="flex justify-center items-center ml-4">
-        <text className="absolute font-serif font-bold text-2xl z-10">B</text>
-        <img src="/GameplayPage/circle_w.svg" className={`${activeButton === "b" ? "filter brightness-90" : ""}`} />
+        <text className="absolute font-serif font-bold text-2xl z-10">B</text>  
+        <img src="/GameplayPage/circle_w.svg" className={`${activeButton === "b" ? "filter brightness-[0.85]" : ""}`} />
       </div>
     </div>
   );
